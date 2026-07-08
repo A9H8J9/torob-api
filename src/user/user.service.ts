e2e7f7b35cfc807b8747a94c39f89f9c29d7e3ee
amirhosein <140063353+A9H8J9@users.noreply.github.com>
2026-07-08 21:31:50 +0330

    fix: add shops to response user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,18 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UserSelectCity } from './user.dto';
+import { ShopType } from '@prisma/client';
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
 
   async me(user_id: number) {
-    return await this.prisma.user.findFirst({
+    const user = await this.prisma.user.findUnique({
       where: {
         id: user_id,
-        is_active: true,
+      },
+      include: {
+        shopMembers: {
+          include: {
+            shop: {
+              select: {
+                id: true,
+                shop_name: true,
+                type: true,
+              },
+            },
+          },
+        },
       },
     });
+
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+
+    const offline_panel_accesses = user.shopMembers
+      .filter((member) => member.shop.type === ShopType.OFFLINE_SHOP)
+      .map((member) => ({
+        id: member.shop.id,
+        name: member.shop.shop_name,
+        permissions: {
+          is_owner: member.is_owner,
+          is_admin: member.is_admin,
+        },
+      }));
+
+    const online_panel_accesses = user.shopMembers
+      .filter((member) => member.shop.type === ShopType.ONLINE_SHOP)
+      .map((member) => ({
+        id: member.shop.id,
+        name: member.shop.shop_name,
+        permissions: {
+          is_owner: member.is_owner,
+          is_admin: member.is_admin,
+        },
+      }));
+
+    return {
+      id: user.id,
+      name: user.name,
+      phone: user.phone,
+
+      offline_panel_accesses,
+      online_panel_accesses,
+    };
   }
 
   async listFavorites(user_id: number) {
