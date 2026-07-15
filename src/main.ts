f053834cc2a874f5424c66cbd162cbee63c79732
amirhosein <140063353+A9H8J9@users.noreply.github.com>
2026-07-15 17:16:39 +0330

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: '*',
+  });
   const config = new DocumentBuilder().setTitle('Torob').setDescription('The torob API description').setVersion('1.0').build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, documentFactory);
